8558a7865ef7098a2a13654f5813867ec2410609
Jake <jakethomson0@gmail.com>
2021-03-19 22:24:27 +0000

    Added start and end date selectors.

--- src/components/Settings.react.js
+++ src/components/Settings.react.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import {ReactComponent as SettingsSVG} from '../images/settings.svg';
 import { Modal, Button } from "react-bootstrap";
+import Datetime from 'react-datetime';
+import moment from 'moment';
+import "react-datetime/css/react-datetime.css";
 import '../css/settings.css';
 
-class News extends Component {
+class Settings extends Component {
   state = {
-    show: false,
+    show: true,
     error: "",
-    submitting: false
+    submitting: false,
+    startDate: moment("2015-01-01"),
+    endDate: moment(),
   }
 
   onRestart = () => {
@@ -19,6 +24,19 @@ class News extends Component {
   setShow = bool => {
     // Sets the visible state of the modal.
     this.setState({ show: bool});
+  }
+
+  handleDateChange = (startDate, event) => {
+    // If it is a valid date, the datepicker will automatically convert it to a datetime object.
+    if(this.state.startDate !== "" && typeof(startDate) === "object") {
+      let adjustedDatetime = moment(this.state.startDate);
+      adjustedDatetime.date(startDate.date());
+      adjustedDatetime.month(startDate.month());
+      adjustedDatetime.year(startDate.year());
+      this.setState({ startDate: adjustedDatetime._d});
+    } else {
+      this.setState({ startDate });
+    }
   }
 
   render() {
@@ -39,10 +57,44 @@ class News extends Component {
               <Modal.Title>Backtest settings</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              <p className="mb-2">Settings will go here.</p>
-              <Button variant="secondary" onClick={handleClose} disabled={true}>
-                Save settings
-              </Button>
+              <form className="container px-0 mx-auto settings-form">
+                <div className="row col-12 mx-0 px-1" >
+                  <div className="col-6 form-group px-1 pr-2">
+                    <label className="px-0 col-form-label settings-form-label" htmlFor="startDate">Start date</label>
+                    <Datetime 
+                      inputProps={{
+                        className:'form-control settings-form-input',
+                        name: "startDate",
+                        id: "startDate",
+                        autoComplete: "off",
+                        readOnly: false
+                      }} 
+                      value={this.state.startDate} 
+                      initialViewDate={this.state.startDate} 
+                      onChange={this.handleDateChange} 
+                      dateFormat="DD-MM-YYYY" 
+                      timeFormat={false} 
+                    />
+                  </div>
+                  <div className="col-6 form-group px-1 pl-2">
+                    <label className="px-0 col-form-label settings-form-label" htmlFor="endDate">End date</label>
+                    <Datetime 
+                      inputProps={{
+                        className:'form-control settings-form-input',
+                        name: "endDate",
+                        id: "endDate",
+                        autoComplete: "off",
+                        readOnly: false
+                      }} 
+                      value={this.state.endDate} 
+                      initialViewDate={this.state.endDate} 
+                      onChange={this.handleDateChange} 
+                      dateFormat="DD-MM-YYYY" 
+                      timeFormat={false} 
+                    />
+                  </div>
+                </div>
+              </form>
             </Modal.Body>
             <Modal.Footer>
               <Button variant="secondary" onClick={handleClose}>
@@ -59,4 +111,4 @@ class News extends Component {
   }
 }
 
-export default News;
+export default Settings;

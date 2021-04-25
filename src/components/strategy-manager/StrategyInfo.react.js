9e013c34219bb97e40f4f0a18c8e91e3a0e80dd4
JakeThomson <jakethomson0@gmail.com>
2021-04-25 15:33:31 +0100

    Added accordian to switch between strategy stats and prev backtests in strategy manager.

--- src/components/strategy-manager/StrategyInfo.react.js
+++ src/components/strategy-manager/StrategyInfo.react.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import '../../css/strategy-manager/strategy-info.css';
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Accordion, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import moment from 'moment';
+import {ReactComponent as ArrowSVG} from '../../images/arrow.svg';
 
 class StrategyInfo extends Component {
   constructor(props) {
@@ -28,7 +29,8 @@ class StrategyInfo extends Component {
     });
 
     this.state = {
-      show: false
+      show: false,
+      activeCardKey: "0"
     }
   }
 
@@ -49,6 +51,10 @@ class StrategyInfo extends Component {
     return true;
   }
 
+  changeCard = () => {
+    this.setState({activeCardKey: this.state.activeCardKey === "0" ? "1" : "0"})
+  }
+
   render() {
     const handleClose = () => this.setShow(false);
     const handleShow = () => this.setShow(true);
@@ -56,8 +62,7 @@ class StrategyInfo extends Component {
     return (
       <div id="selection-config-container" className="container py-2 px-3">
         <h5 className="row col-12 strategy-editor-header">Strategy info</h5>
-        <div id="strategy-info-container" className="py-1">
-          {
+                  {
             this.props.selected ?
               <div className="container row mx-auto px-0">
                 <div className="px-0 mx-auto mb-2">
@@ -65,7 +70,7 @@ class StrategyInfo extends Component {
                   <Link 
                     to={{
                         pathname: "/strategy-editor",
-                        state: {...this.props.selected, action: "edit"} 
+                        state: {strategyId: this.props.selected.strategyId, strategyName: this.props.selected.strategyName, technicalAnalysis: this.props.selected.technicalAnalysis, action: "edit"} 
                       }} 
                     className="mx-auto my-3" 
                     onMouseDown={e => e.preventDefault()} 
@@ -98,34 +103,56 @@ class StrategyInfo extends Component {
                   </Modal>
                 </div>
                 
-                {!this.checkValid() ? <div className="col-12 px-0 mb-1 text-center text-danger"><i>Strategy contains an invalid module</i></div> : null}
-                { 
-                  this.props.selected.active 
-                  ?
-                    <div className="col-12 px-0 mb-1 text-center" style={{fontSize: "10pt"}}><i>Backtest has been running for 00:38:05</i></div>
-                  : 
-                    this.props.selected.backtests[0] === undefined 
-                    ?
-                      <div className="col-12 px-0 mb-1 text-center" style={{fontSize: "10pt"}}><i>Strategy has not yet run a full backtest</i></div>
-                    :
-                      <div className="col-12 px-0 mb-1 text-center" style={{fontSize: "10pt"}}><i>Backtest last run {this.props.selected.backtests[0].datetimeFinished.fromNow()}</i></div>
-                }
-                <div className="col-12 px-0 text-center">
-                  <h6 className="mb-0 mt-2">Selected Analysis Modules:</h6>
-                  <p className="mb--1">{this.props.selected.technicalAnalysis.map(a => a.name).join(", ")}</p>
-                </div>
-                <div className="col-6 px-0 text-center">
-                  <h2 className="mb-0">{this.props.selected.avgSuccess === "N/A" ? this.props.selected.avgSuccess : this.props.selected.avgSuccess + "%"}</h2>
-                  <h6>Avg. success rate</h6>
-                </div>
-                <div className="col-6 px-0 text-center">
-                  <h2 className="mb-0">{this.props.selected.avgReturns === "N/A" ? this.props.selected.avgReturns : "+" + this.props.selected.avgReturns + "%"}</h2>
-                  <h6>Avg. returns</h6>
-                </div>
+        <div id="strategy-info-container" className="py-1">
+                
+                
+                <Accordion activeKey={this.state.activeCardKey || "0"} style={{minWidth: "100%"}}>
+                  <Card style={{border: 0}}>
+                    <Accordion.Collapse eventKey="0">
+                      <Card.Body className="container row p-0 mx-auto mb-1">
+                        {!this.checkValid() ? <div className="col-12 px-0 mb-1 text-center text-danger"><i>Strategy contains an invalid module</i></div> : null}
+                        { 
+                          this.props.selected.active 
+                          ?
+                            <div className="col-12 px-0 mb-1 text-center" style={{fontSize: "10pt"}}><i>Backtest has been running for 00:38:05</i></div>
+                          : 
+                            this.props.selected.backtests[0] === undefined 
+                            ?
+                              <div className="col-12 px-0 mb-1 text-center" style={{fontSize: "10pt"}}><i>Strategy has not yet run a full backtest</i></div>
+                            :
+                              <div className="col-12 px-0 mb-1 text-center" style={{fontSize: "10pt"}}><i>Backtest last run {this.props.selected.backtests[0].datetimeFinished.fromNow()}</i></div>
+                        }
+                        <div className="col-12 px-0 text-center">
+                          <h6 className="mb-0 mt-2">Selected Analysis Modules:</h6>
+                          <p className="mb--1">{this.props.selected.technicalAnalysis.map(a => a.name).join(", ")}</p>
+                        </div>
+                        <div className="col-6 px-0 text-center">
+                          <h2 className="mb-0">{this.props.selected.avgSuccess === "N/A" ? this.props.selected.avgSuccess : this.props.selected.avgSuccess + "%"}</h2>
+                          <h6>Avg. success rate</h6>
+                        </div>
+                        <div className="col-6 px-0 text-center">
+                          <h2 className="mb-0">{this.props.selected.avgReturns === "N/A" ? this.props.selected.avgReturns : "+" + this.props.selected.avgReturns + "%"}</h2>
+                          <h6>Avg. returns</h6>
+                        </div>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                  <Card style={{border: 0}}>
+                    <Card.Header className="p-0" style={{border: 0}}>
+                      <Accordion.Toggle style={{border: 0}} className="container row mx-auto px-3 py-2" as={Card.Header} variant="link" eventKey="1" onClick={this.changeCard}>
+                        {this.state.activeCardKey === "0" ? "Show Past Backtests" : "Show Strategy Summary"}
+                        <ArrowSVG id="arrow-icon" className={"my-auto mr-1 ml-auto" + (this.state.activeCardKey === "0" ? " flip" : "")} />
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                      <Card.Body>Hello! I'm another body</Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
               </div>
+        </div>
             : null
           }
-        </div>
       </div>
     )
   }
